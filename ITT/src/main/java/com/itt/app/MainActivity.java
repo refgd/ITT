a10d94974f6a0e6898ea349d24d9a60f9b54a075
Jack Conway <j124519951@gmail.com>
2014-05-13 20:02:28 -0400

    first commit

---
+++ ITT/src/main/java/com/itt/app/MainActivity.java
@@ -0,0 +1,206 @@
+package com.itt.app;
+
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
+public class MainActivity extends ActionBarActivity implements LocationListener {
+
+    private LocationManager locationManager;
+    private TextView infoOut;
+    private TextView infoSend;
+    private TextView infoRecv;
+
+    private String provider;
+
+    private String imei;
+
+    private String TAG = "socket thread";
+    
+    SocThread socketThread;
+
+    Handler mhandler;
+    Handler mhandlerSend;
+    private Context ctx;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ctx = MainActivity.this;
+
+        infoOut = (TextView) findViewById(R.id.infoTxt);
+        infoSend = (TextView) findViewById(R.id.sendTxt);
+        infoRecv = (TextView) findViewById(R.id.respTxt);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        TelephonyManager mngr = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        imei = mngr.getDeviceId();
+
+        boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        // check if enabled and if not send user to the GSP settings
+        // Better solution would be to display a dialog and suggesting to
+        // go to the settings
+        if (!enabled) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+
+        // Define the criteria how to select the locatioin provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        // Initialize the location fields
+        if (location != null) {
+            onLocationChanged(location);
+        } else {
+            infoOut.setText("Location not available");
+        }
+
+        mhandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                try {
+                    Log.i(TAG, "mhandler: msg=" + msg.what);
+                    if (msg.obj != null) {
+                        String s = msg.obj.toString();
+                        if (s.trim().length() > 0) {
+                            Log.i(TAG, "mhandler: obj=" + s);
+                            infoRecv.setText("Server:" + s);
+                        } else {
+                            Log.i(TAG, "no data");
+                        }
+                    }
+                } catch (Exception ee) {
+                    Log.i(TAG, "loading error");
+                    ee.printStackTrace();
+                }
+            }
+        };
+        mhandlerSend = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                try {
+                    Log.i(TAG, "mhandlerSend: msg.what=" + msg.what);
+                    String s = msg.obj.toString();
+                    if (msg.what == 1) {
+                        Log.i(TAG, "ME: " + s + "\nsend success");
+                    } else {
+                        Log.i(TAG, "ME: " + s + "\nsend fail");
+                    }
+                } catch (Exception ee) {
+                    Log.i(TAG, "loading error");
+                    ee.printStackTrace();
+                }
+            }
+        };
+        startSocket();
+
+        Button btnsend = (Button) findViewById(R.id.btn_send);
+
+        btnsend.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                //Log.i(TAG, "Sending data");
+
+                String str = infoSend.getText().toString();
+
+                socketThread.Send(str);
+
+            }
+        });
+
+    }
+
+    public void startSocket() {
+        socketThread = new SocThread(mhandler, mhandlerSend, ctx);
+        socketThread.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Double lati = location.getLatitude();
+        Double loit = location.getLongitude();
+        SimpleDateFormat s = new SimpleDateFormat("yyMMddHHmmss");
+        String ms = s.format(new Date());
+        Log.i("TIME", ms);
+        String outData = imei+",AAA,35,"+lati+","+loit+","+ms+",A,10,11,0,217,1.1,37,36118,846208,310|260|7DA1|8B2B,0000,000A|0002||02D6|00FE,*A7\r\n";
+        String pData = "$$g" + outData.length() + "," + outData;
+        infoSend.setText(pData);
+
+        String str = "Latitude: "+lati+"\nLongitude: "+loit+"\nimei: "+imei;
+        infoOut.setText(str);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ ITT/src/main/java/com/itt/app/SocThread.java
@@ -0,0 +1,167 @@
+package com.itt.app;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+public class SocThread extends Thread {
+    private String ip = "50.195.116.150";
+    //private String ip = "131.125.78.63";
+    private int port = 8500;
+    private String TAG = "socket thread";
+    private int timeout = 10000;
+
+    public Socket client = null;
+    PrintWriter out;
+    BufferedReader in;
+    public boolean isRun = true;
+    Handler inHandler;
+    Handler outHandler;
+    Context ctx;
+    private String TAG1 = "===Send===";
+    SharedPreferences sp;
+
+    public SocThread(Handler handlerin, Handler handlerout, Context context) {
+        inHandler = handlerin;
+        outHandler = handlerout;
+        ctx = context;
+        Log.i(TAG, "create socket thread");
+    }
+
+    /**
+     * 连接socket服务器
+     */
+    public void conn() {
+
+        try {
+            initdate();
+            Log.i(TAG, "connect……");
+            client = new Socket(ip, port);
+            client.setSoTimeout(timeout);
+            Log.i(TAG, "connect success");
+            in = new BufferedReader(new InputStreamReader(
+                    client.getInputStream()));
+            out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
+                    client.getOutputStream())), true);
+            Log.i(TAG, "Output stream created ");
+        } catch (UnknownHostException e) {
+            Log.i(TAG, "Connect error: UnknownHostException");
+            e.printStackTrace();
+            conn();
+        } catch (IOException e) {
+            Log.i(TAG, "Connect io error");
+            e.printStackTrace();
+        } catch (Exception e) {
+            Log.i(TAG, "Connect error Exception" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void initdate() {
+        sp = ctx.getSharedPreferences("SP", ctx.MODE_PRIVATE);
+        ip = sp.getString("ipstr", ip);
+        port = Integer.parseInt(sp.getString("port", String.valueOf(port)));
+        Log.i(TAG, "get ip port:" + ip + ";" + port);
+    }
+
+    /**
+     * get data
+     */
+    @Override
+    public void run() {
+        Log.i(TAG, "socket start");
+        conn();
+        Log.i(TAG, "1.run");
+        String line = "";
+        while (isRun) {
+            try {
+                if (client != null) {
+                    Log.i(TAG, "2.check data");
+                    while ((line = in.readLine()) != null) {
+                        Log.i(TAG, "3.getdata" + line + " len=" + line.length());
+                        Log.i(TAG, "4.start set Message");
+                        Message msg = inHandler.obtainMessage();
+                        msg.obj = line;
+                        inHandler.sendMessage(msg);
+                        Log.i(TAG1, "5.send to handler");
+                    }
+
+                } else {
+                    Log.i(TAG, "no connection");
+                    conn();
+                }
+            } catch (Exception e) {
+                Log.i(TAG, "receive data error: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * send data
+     *
+     * @param mess
+     */
+    public void Send(String mess) {
+        try {
+            if (client != null) {
+                Log.i(TAG1, "send to " + mess
+                        + client.getInetAddress().getHostAddress() + ":"
+                        + String.valueOf(client.getPort()));
+                out.println(mess);
+                out.flush();
+                Log.i(TAG1, "send success");
+                Message msg = outHandler.obtainMessage();
+                msg.obj = mess;
+                msg.what = 1;
+                outHandler.sendMessage(msg);
+            } else {
+                Log.i(TAG, "client not exist");
+                Message msg = outHandler.obtainMessage();
+                msg.obj = mess;
+                msg.what = 0;
+                outHandler.sendMessage(msg);
+                Log.i(TAG, "connect not exist");
+                conn();
+            }
+
+        } catch (Exception e) {
+            Log.i(TAG1, "send error");
+            e.printStackTrace();
+        } finally {
+            Log.i(TAG1, "send done");
+
+        }
+    }
+
+    /**
+     * close connect
+     */
+    public void close() {
+        try {
+            if (client != null) {
+                Log.i(TAG, "close in");
+                in.close();
+                Log.i(TAG, "close out");
+                out.close();
+                Log.i(TAG, "close client");
+                client.close();
+            }
+        } catch (Exception e) {
+            Log.i(TAG, "close err");
+            e.printStackTrace();
+        }
+
+    }
+}
